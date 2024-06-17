37bd3a6b4c3df4be9a001068ba831ccea15ea923
Pocobeer <vall.com3454.71@gmail.com>
2024-06-17 19:37:13 +0300

    user list + tablemarks

--- static/js/booking.js
+++ static/js/booking.js
@@ -39,12 +39,13 @@ function displayResult(){
     </ul>`; 
 }
 // переменная массив участников
-var participants = [];
+
+var participants = [currentuser];
 // функция добавления участника
 function addParticipant() {
     // получение значения из поля ввода
     var studentIdInput = document.getElementById("student-id");
-    var studentId = studentIdInput.value;
+    var studentId = studentIdInput.value.trim();
     // проверка на пустоту
     if(studentId) {
        //participants.push(studentId);
@@ -60,9 +61,18 @@ function displayParticipants(){
     var participantsElement = document.getElementById("participants-result");
     participantsElement.innerHTML = `<p>Добавленные участники:</p>
     <ul>
-        ${participants.map(participant => `<li>${participant}</li>`).join('')}
+        ${participants.map((participant,index) => {
+            if(participant == currentuser){
+                return `<li>${participant} (Вы)</li>`;
+            }else{
+                return `<li>${participant}</li>`;
+            }
+        }).join('')}
     </ul>`;
 }
+window.onload = function() {
+    displayParticipants();
+};
 // определение переменных под столики
 var table_1 = document.getElementById("table_1");
 var table_2 = document.getElementById("table_2");
@@ -77,34 +87,37 @@ var popupInterval = document.querySelector(".popup-interval");
 var selectedTable = 0;
 // переменная выбранных интервалов
 var selectedIntervals = [];
+function selectTable(TableId){
+    selectedTable = TableId;
+    popupInterval.style.display = "block";
+
+    var allTables = document.querySelectorAll(".square, .squarex2, .squarex3");
+    allTables.forEach(table => table.classList.remove("selected"));
+
+    var selectedTable = document.getElementById(`table_${TableId}`);
+    selectedTable.classList.add("selected");
+}
 // функции выбора столика
 table_1.addEventListener("click", function(){
-    selectedTable = 1;
-    popupInterval.style.display = "block";
+    selectTable(1);
 });
 table_2.addEventListener("click", function(){
-    selectedTable = 2;
-    popupInterval.style.display = "block";
+    selectTable(2);
 });
 table_3.addEventListener("click", function(){
-    selectedTable = 3;
-    popupInterval.style.display = "block";
+    selectTable(3);
 });
 table_4.addEventListener("click", function(){
-    selectedTable = 4;
-    popupInterval.style.display = "block";
+    selectTable(4);
 });
 table_5.addEventListener("click", function(){
-    selectedTable = 5;
-    popupInterval.style.display = "block";
+    selectTable(5);
 });
 table_6.addEventListener("click", function(){
-    selectedTable = 6;
-    popupInterval.style.display = "block";
+    selectTable(6);
 });
 table_7.addEventListener("click", function(){
-    selectedTable = 7;
-    popupInterval.style.display = "block";
+    selectTable(7);
 });
 //var closeButton = document.querySelector(".popup button");
 // функция закрытия всплывающего окна интервалов

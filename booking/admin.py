8746b7cd9ad603cac5ed4f92f59f08700c2bbeca
tr0ublekat <kirillkojev8@gmail.com>
2024-06-29 13:06:15 +0300

    added test data into Room and Institutions

--- booking/admin.py
+++ booking/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Intstitution, Room
 
 admin.site.register(Book)
+admin.site.register(Intstitution)
+admin.site.register(Room)

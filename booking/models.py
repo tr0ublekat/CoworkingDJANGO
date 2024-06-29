87c254bf89792df6ea8c41c1ab74784cfdfd78e6
tr0ublekat <kirillkojev8@gmail.com>
2024-06-29 12:14:48 +0300

    added institution and room models

--- booking/models.py
+++ booking/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from users.models import CustomUser
 
+class Intstitution(models.Model):
+    name = models.CharField(max_length=100)
+    adress = models.CharField(max_length=150)
+
+class Room(models.Model):
+    number = models.IntegerField()
+    institution = models.ForeignKey(Intstitution, on_delete=models.CASCADE)
+
 class Book(models.Model):
     data = models.DateField()
     place = models.IntegerField()
